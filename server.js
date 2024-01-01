30d89c2bc4a645311b612a21f45f016a6b21c97f
andrew <trolevan26299@gmail.com>
2024-01-01 14:37:31 +0700

    structure code

---
+++ server.js
@@ -0,0 +1,11 @@
+const app = require("./src/app");
+
+const PORT = 6969;
+
+const server = app.listen(PORT, () => {
+  console.log(`eCommerce start with port : ${PORT}`);
+});
+
+// process.env("SIGINT", () => {
+//   server.close(() => console.log("Exit Server Express"));
+// });
---
+++ src/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const morgan = require("morgan");
+const { default: helmet } = require("helmet");
+const compression = require("compression");
+const app = express();
+
+// init middleware
+app.use(morgan("dev")); // show logs when client send request
+app.use(helmet()); // help security
+app.use(compression()); // help save bandwidth
+
+// init DB
+
+// init router
+app.get("/", (req, res, next) => {
+  return res.status(200).json({
+    message: "Welcome to Andrew",
+  });
+});
+
+// handling error
+
+module.exports = app;
